f04fc33dfc2058bc865fd3d2d33d71cf93eb4f82
ocuil <alejandrosl@gmail.com>
2021-02-06 17:03:54 +0000

    [update patch script][use only if you do a go mod vendor]

--- patchVendors.sh
+++ patchVendors.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 [ ! -d "/path/to/dir" ] && cp -aR CHeaders ./vendor/github.com/ibm-messaging/mq-golang/v5/ibmmq/includes
+
+find ./vendor/github.com/ibm-messaging/mq-golang/v5/ibmmq/ -type f -name "mqi*" -exec \
+    sed -i -e 's%<cmqc.h>%"includes/cmqc.h"%g' {} +
+
+find ./vendor/github.com/ibm-messaging/mq-golang/v5/ibmmq/ -type f -name "mqi*" -exec \
+    sed -i -e 's%<cmqxc.h>%"includes/cmqxc.h"%g' {} +
+    
+find ./vendor/github.com/ibm-messaging/mq-golang/v5/ibmmq/ -type f -name "mqi*" -exec \
+    sed -i -e 's%<cmqcfc.h>%"includes/cmqcfc.h"%g' {} +
+
+find ./vendor/github.com/ibm-messaging/mq-golang/v5/ibmmq/ -type f -name "mqi*" -exec \
+    sed -i -e 's%<cmqstrc.h>%"includes/cmqstrc.h"%g' {} +
